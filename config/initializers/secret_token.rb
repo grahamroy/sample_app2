bef16e2e7edc0722cc876c7bdf91fc1aa6369b54
graham roy <grahamroy@hotmail.com>
2011-01-11 17:07:00 +0000

    initial commit - moved to postgres for heroku purposes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp2::Application.config.secret_token = '4aaa1f85e121335ffaf199c29c85adb41c970e4e57d5c3ed76629d535945da5e18e61b2410232efc1ef2ef9e7281cc5b40bd886ae40d61ca2597bce545ad87f5'
